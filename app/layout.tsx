92a6ec5331ff51c580e5f58658131659ed2772df
fahmialashri <2210631170067@student.unsika.ac.id>
2026-02-18 04:20:16 +0700

    update ui landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,11 @@ import NavbarWrapper from "@/components/layout/NavbarWrapper";
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter", display: "swap" });
 const playfair = Playfair_Display({ subsets: ["latin"], variable: "--font-playfair", display: "swap" });
 
-// Root Layout SEKARANG ADALAH SERVER COMPONENT (Tanpa "use client")
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="id" className={`${inter.variable} ${playfair.variable}`}>
       <body className="antialiased selection:bg-[#FF85A2]/30">
-        {/* Pindahkan logika pengecekan "/" ke dalam sini */}
         <NavbarWrapper>
           {children}
         </NavbarWrapper>
